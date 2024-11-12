0eb1078025969e6b323fdbe4502a1202ac72a218
Alex Araujo <alexsaspbr@gmail.com>
2024-11-11 22:09:38 -0300

    implantando com docker

--- src/main/java/tech/ada/products_api/config/SecurityConfiguration.java
+++ src/main/java/tech/ada/products_api/config/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize ->
                         authorize
-                                .requestMatchers("/h2-console/**", "/api-docs/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html","/**.html" ).permitAll()
+                                .requestMatchers("/api-docs/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html","/**.html" ).permitAll()
                                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                                 .requestMatchers(PathRequest.toH2Console()).permitAll()
